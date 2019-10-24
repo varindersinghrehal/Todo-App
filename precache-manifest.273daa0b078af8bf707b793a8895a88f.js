9a7f2efc4e8fd9f75a1594017ad0419e56b65f13
Varinder Singh Rehal <varinderbabbu555@gmail.com>
2019-10-24 16:32:42 +0200

    Updates

---
+++ precache-manifest.273daa0b078af8bf707b793a8895a88f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828e469fb3b0502a5dfc3bc08bea6075",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "a2a3989cc2e1583c3d8a",
+    "url": "/todoapp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32e84a28151df82831b2",
+    "url": "/todoapp/static/js/2.dc4140d3.chunk.js"
+  },
+  {
+    "revision": "a2a3989cc2e1583c3d8a",
+    "url": "/todoapp/static/js/main.da14b04c.chunk.js"
+  },
+  {
+    "revision": "80f8f6c5b874c7fc3aeb",
+    "url": "/todoapp/static/js/runtime-main.e9ca35db.js"
+  }
+]);
